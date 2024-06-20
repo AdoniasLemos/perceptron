93f037f30f142541f7ca18d4c576c34c98aa87aa
Adonias Silva <adoniaslg@gmail.com>
2024-06-20 16:56:54 -0300

    Criacao do formulario para testar a predicao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export interface Element {
   inputs: number[];
@@ -22,7 +25,7 @@ const LEARNING_RATE = 0.3;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MatButtonModule, MatTableModule, MatCardModule, MatGridListModule],
+  imports: [RouterOutlet, CommonModule, MatButtonModule, MatTableModule, MatCardModule, MatGridListModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -32,34 +35,58 @@ export class AppComponent {
 
   dataSource = ELEMENT_DATA;
 
-
   weights = [1, 1, 1];
+
+
+  calculatorForm: FormGroup;
+
+  result: number | null = null;
+
+  constructor(private fb: FormBuilder) {
+    this.calculatorForm = this.fb.group({
+      input1: [null],
+      input2: [null]
+    });
+  }
+
+  calculate() {
+    const input1 = this.calculatorForm.value.input1;
+    const input2 = this.calculatorForm.value.input2;
+
+    console.log("input1 ", input1)
+
+    console.log("input2 ", input2)
+    if (input1 !== null && input2 !== null) {
+      console.log(this.predict([input1, input2, 1]))
+      this.result = this.predict([input1, input2, 1]);
+    }
+  }
 
   activationFunction(sum: number): number {
     return sum >= 0 ? 1 : -1;
   }
 
-
-
-  error(valorDesejado: number, valorObtido: number) {
-    return valorDesejado - valorObtido;
+  error(desiredValue: number, predictedValue: number) {
+    return desiredValue - predictedValue;
   }
 
   train() {
-    for (let i = 0; i < this.dataSource.length; i++) {
-      let inputs = this.dataSource[i].inputs;
-      let prediction = this.predict(this.dataSource[i].inputs);
-      let valorDesejado = this.dataSource[i].output
-      let error = this.error(valorDesejado, prediction);
+    for (let e = 0; e <= 10; e++) {
+      for (let i = 0; i < this.dataSource.length; i++) {
+        let inputs = this.dataSource[i].inputs;
+        let prediction = this.predict(this.dataSource[i].inputs);
+        let desiredValue = this.dataSource[i].output
+        let error = this.error(desiredValue, prediction);
 
-      while (error != 0) {
-        console.log(this.weights)
-        console.log(i)
-        this.adjustWeights(error, inputs);
+        while (error != 0) {
+          console.log(this.weights)
+          console.log(i)
+          this.adjustWeights(error, inputs);
 
-        prediction = this.predict(inputs)
+          prediction = this.predict(inputs)
 
-        error = this.error(valorDesejado, prediction)
+          error = this.error(desiredValue, prediction)
+        }
       }
     }
   }
